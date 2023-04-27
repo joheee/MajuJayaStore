a767acdf442baed8725541d686e835be2554c609
joheee <94522173+joheee@users.noreply.github.com>
2023-04-27 23:56:52 +0700

    fix layout login page

--- view/auth/login.php
+++ view/auth/login.php
@@ -1,5 +1,4 @@
-<?php require_once '../../utils/includeTemplate.php'; ?>
-
+<?= require_once '../../utils/includeTemplate.php'; ?>
 
 <?= include_template('../../view/template/header.php', ['title' => 'Login | MajuJaya']) ?>
 
@@ -13,23 +12,14 @@
             <h3 class="mb-5">Sign in</h3>
 
             <div class="form-outline mb-4">
-              <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
-              <label class="form-label" for="typeEmailX-2">Email</label>
+              <input type="email" id="form2Example17" class="form-control form-control-lg" placeholder="email" name="email">
             </div>
 
             <div class="form-outline mb-4">
-              <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
-              <label class="form-label" for="typePasswordX-2">Password</label>
+              <input type="password" id="form2Example27" class="form-control form-control-lg" placeholder="password" name="password"/>
             </div>
 
             <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
-
-            <hr class="my-4">
-
-            <button class="btn btn-lg btn-block btn-primary" style="background-color: #dd4b39;"
-              type="submit"><i class="fab fa-google me-2"></i> Sign in with google</button>
-            <button class="btn btn-lg btn-block btn-primary mb-2" style="background-color: #3b5998;"
-              type="submit"><i class="fab fa-facebook-f me-2"></i>Sign in with facebook</button>
 
           </div>
         </div>
@@ -38,4 +28,4 @@
   </div>
 </section>
 
-<?= include_template('../../view/template/header.php') ?>
+<?= include_template('../../view/template/footer.php') ?>
--- view/template/header.php
+++ view/template/header.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
@@ -16,9 +16,13 @@
     rel="stylesheet"/>
     <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&family=Pacifico&family=Poppins:ital,wght@1,600&display=swap" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/album/">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-
+    
     <title><?php echo $title ?></title>
 </head>
 <body>
