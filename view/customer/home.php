2c22faa38a609a553e9f60e7f9d5cb7824b33fc4
joheee <94522173+joheee@users.noreply.github.com>
2023-04-28 17:07:28 +0700

    product page for admin

--- view/customer/home.php
+++ view/customer/home.php
@@ -1,1 +1,11 @@
+<?php require_once '../../utils/includeTemplate.php'; ?>
+<?= include_template('../../view/template/header.php', ['title' => 'Home | MajuJaya']) ?>
+
+<?php 
+
+
+?>
+
 this is home php
+
+<?= include_template('../../view/template/footer.php') ?>
